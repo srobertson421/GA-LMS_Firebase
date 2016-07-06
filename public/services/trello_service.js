e49c006a2fefe19bb691a46dffdf44ea29a227d2
srobertson421 <srobertson421@gmail.com>
2016-07-06 12:10:03 -0700

    Calendar wired to Trello. Bugs with loading projects and calendar

--- public/services/trello_service.js
+++ public/services/trello_service.js
@@ -8,31 +8,16 @@ angular.module('TrelloService', [])
       read: true,
       write: true },
     expiration: 'never',
-    success: this.authenticationSuccess,
-    error: this.authenticationFailure
+    success: function() {
+      console.log('Trello auth success');
+    },
+    error: function() {
+      console.log('Trello auth failure');
+    }
   });
 })
 
 .service('Trello', [function() {
-
-  this.authSuccess = function() {
-    console.log('Trello auth success');
-    console.log(this);
-  }
-
-  this.authFailure = function() {
-    console.log('Trello auth failure');
-  }
-
-  this.initCalendar = function(callback) {
-    Trello.get('boards/57179c9c2c05a97c4b22e2d2/cards', function success(response) {
-      //console.log(response);
-      callback(null, response);
-    }, function error(error) {
-      //console.log(error);
-      callback(error, null);
-    });
-  }
 
   this.getBoards = function() {
     Trello.get('members/me/boards', function success(response) {
